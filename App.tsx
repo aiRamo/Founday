dc63deccb47877797a31921c9ca2f8110d193148
future3004 <richardmel@outlook.com>
2023-02-19 06:55:57 -0600

    Added navigation between screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './Components/Home';
+import Login from './Components/Login';
+
+const Stack = createNativeStackNavigator();
 
  const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Signup"
+        component={Login}
+        options={{title: 'Welcome to Founday'}}
+      />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
