c39f7d027402bbad4e4be8fb3b3d958b669fc666
Kyle Henry <kylehenry7000@gmail.com>
2023-02-18 21:44:18 -0600

    Added Some Blank Pages

---
+++ Components/Login.tsx
@@ -0,0 +1,34 @@
+//React Native Button
+//https://aboutreact.com/react-native-button/
+
+//import React in our code
+import React from 'react';
+
+//import all the components we are going to use
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+
+const Login = () => {
+  const onPressLearnMore = () => {
+    //For generating alert on buttton click
+    alert('Hello');
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* enclose all components in this View tag */}
+      <View style={styles.container}>
+        <Text>Button Example</Text>
+        {/* Button whith handler function named onPressLearnMore*/}
+        <Button onPress={onPressLearnMore} title="Click Me" color="#841584" />
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default Login;
