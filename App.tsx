d01b79ef34ad4c2d3f2160fb5670bd2c29efd537
Adrian Ramos <airria2000@gmail.com>
2023-02-20 16:18:43 -0600

    Minor bug fixes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
-import SignUp from './Components/SignUp';
-import { Divider } from '@rneui/themed';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
